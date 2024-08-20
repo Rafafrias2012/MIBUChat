fad4755e416fa63cec6b148ba829982fbc7701eb
FafaelDuarte <155685529+Rafafrias2012@users.noreply.github.com>
2024-08-20 09:20:05 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,10 +2,9 @@ const express = require('express');
 const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
+const path = require('path');
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
-});
+app.use(express.static(path.join(__dirname, 'frontend')));
 
 const buddies = new Set();
 
